45438a3dced3347fc8b416fe93ec5065ee624d55
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2015-01-05 14:30:44 +0100

    removed unused code and commented

--- player.js
+++ player.js
@@ -1,10 +1,10 @@
 var Player = {
+    // dom elements; set on load
     "pl": null, 
-    "volumeInfo": null,
     "timeInfo": null,
     "timeInterval": undefined,
     
-    "volume": 0,
+    "volume": 5,
 
     // jumps to a position given by an integer representing a percentage
     "setTime": function(s) {
@@ -37,15 +37,12 @@ var Player = {
 
 	this.pl.volume = this.volume / 10;
 
-	if (this.volumeInfo !== null)
-	    this.volumeInfo.textContent = this.volume;
-	
 	document.getElementById("vBar").style.width
 	    = this.volume * 10 + "%";
 	
     },
 
-    // updates time in the gui
+    // updates audio time progression in the gui
     "updTime": function() {
 	var ratio = this.pl.currentTime / this.pl.duration;
 	var perc = Math.floor(ratio * 10000) / 100  + "%";
@@ -55,20 +52,21 @@ var Player = {
 	document.getElementById("pb").style.width = perc;
     },
 
-    // hooks the time updates to every 10 ms
+    // hooks the audio time progression updates to every 10 ms
     "hookTimeUpdates": function() {
 	var that = this; 
 	this.timeInterval = setInterval(Player.updTime, 10);
     },
-    
+
+    // clear audio time progression updates
     "clearTimeUpdates": function() {
 	clearInterval(this.timeInterval);
     },
     
-    
+
+    // initializes player
     "init": function() {
 	this.pl = document.getElementById("player");
-	this.volumeInfo = document.getElementById("vol")
 	this.timeInfo = document.getElementById("time");
 	
 	if(this.pl === null) {
@@ -79,48 +77,57 @@ var Player = {
 	pl = this.pl;
 	
 	bindClicks({
+	    // next song
+	    "next": function(e) {
+		alert("Not implemented");
+	    },
+	    // previous song
+	    "prev": function(e) {
+		alert("Not implemented");
+	    },
+	    // play stopped or paused song 
 	    "play": function(e) {
 		e.stopPropagation();
 		Player.hookTimeUpdates();
 		pl.play();
 	    },
+	    // play/pause toggle
 	    "pause": function(e) {
 		e.stopPropagation();
 		Player.clearTimeUpdates();
 		pl.pause();
 	    },
+	    // stops song and rewinds to start
 	    "stop": function(e) {
 		e.stopPropagation();
 		Player.clearTimeUpdates();
 		pl.pause();
 		Player.setTime(0);
 	    },
+	    // increases volume by 1 step of 10
 	    "incVol": function(e) {
 		e.stopPropagation();
 		Player.incVol();
 		Player.updVol();
 	    },
-	
+	    // decreases volume
 	    "decVol": function(e) {
 		e.stopPropagation();
 		Player.decVol();
 		Player.updVol();
+	    },
+	    // jumps to a specific segment in song progression
+	    "pbContainer": function(e) {
+		e.stopPropagation();
+		var x = e.pageX - this.offsetLeft + 1;
+		var p = Math.floor(x  / this.offsetWidth * 100);
+		document.getElementById("pb").style.width = p + "%";
+		Player.setTime(p);
 	    }
-
 	});
-
-	document.getElementById("pbContainer").onclick = function(e) {
-	    e.stopPropagation();
-	    var x = e.pageX - this.offsetLeft + 1;
-	    var p = Math.floor(x  / this.offsetWidth * 100);
-	    document.getElementById("pb").style.width = p + "%";
-	    Player.setTime(p);
-	}
-
     }
-    
-    
 };
+
 window.onload = function() {
     Player.init();
 }
